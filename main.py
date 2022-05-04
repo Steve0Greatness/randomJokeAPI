4197051c39dc4e85d8a51c4b48e79b7ddc91f68c
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2022-05-04 21:55:51 +0000

    coding 👍

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 from flask import Flask, send_file
-import random
+from random import choice
 
 app = Flask(__name__)
 
 # list of jokes
-with open('jokes.txt') as file:
-	jokes = file.read().split('\n')
+with open('jokes.txt') as f:
+	jokes = f.read().split('\n')
 
 @app.route('/')
-def home():
-  return { 'joke': random.choice(jokes) }
+def json():
+  return { 'joke': choice(jokes) }
 
 @app.route('/txt')
-def plain():
-  return random.choice(jokes)
+def text():
+  return choice(jokes)
 
 @app.route('/docs')
 def docs():
   return send_file('docs.html')
 
-def run():
-  app.run(host='0.0.0.0',port=8080)
-
 if __name__ == "__main__":
-	run()
+	app.run('0.0.0.0')
